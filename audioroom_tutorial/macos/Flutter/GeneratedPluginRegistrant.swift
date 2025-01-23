e1f8fc42889d1c7d7159558e4b2eafbfb573aab9
Neevash Ramdial (Nash) <mail@neevash.dev>
2025-01-23 13:18:01 -0700

    Fix audio rooms build iOS

--- audioroom_tutorial/macos/Flutter/GeneratedPluginRegistrant.swift
+++ audioroom_tutorial/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,14 +6,16 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import device_info_plus
 import firebase_core
 import firebase_messaging
 import flutter_webrtc
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
