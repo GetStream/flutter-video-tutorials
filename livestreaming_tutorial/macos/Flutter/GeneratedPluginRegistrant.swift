10958e6f97ec1000431db2cfff4b97494a8c2322
Maciej Brażewicz <maciej.brazewicz@getstream.io>
2026-06-03 14:44:28 +0200

    v1.4.0 adjustments

--- livestreaming_tutorial/macos/Flutter/GeneratedPluginRegistrant.swift
+++ livestreaming_tutorial/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import connectivity_plus
 import device_info_plus
 import flutter_secure_storage_macos
 import package_info_plus
-import path_provider_foundation
 import sqflite_darwin
 import stream_webrtc_flutter
 
@@ -20,7 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
 }
